d7640e136f2ee9d6d4d7b30fbb3b41a46cc0e70c
DESKTOP-03O8QEC\Morgan <morgan.espindola@gmail.com>
2017-10-02 18:57:55 -0300

    Class 9
    set the slideshow background, subjects, subtitles and the effect of change slides

--- code/assets/js/script.js
+++ code/assets/js/script.js
@@ -1,7 +1,30 @@
 window.onload = function() {
+
+	setInterval(autoChangeSlide, 10000); // Function to change slides on the site
+
+	// Get and change the side of slides by the size of the slideshow class
 	var slideshowWidth = document.getElementById("slideshow").offsetWidth;
 	var slides = document.getElementsByClassName("slide");
 	for (var i in slides) {
 		slides[i].style.width = slideshowWidth + "px";
 	}
+}
+
+var slideItem = 0;
+
+function autoChangeSlide() {
+	var slideshowWidth = document.getElementById("slideshow").offsetWidth;
+	if (slideItem >= 3) {
+		slideItem = 0;
+	} else {
+		slideItem ++;
+	}
+
+	document.getElementsByClassName("slideshow-area")[0].style.marginLeft = "-" + (slideshowWidth * slideItem) + "px";
+}
+
+function manualChangeSlide(pos) {
+	slideItem = pos;
+	var slideshowWidth = document.getElementById("slideshow").offsetWidth;
+	document.getElementsByClassName("slideshow-area")[0].style.marginLeft = "-" + (slideshowWidth * slideItem) + "px";
 }
