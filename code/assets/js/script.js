d4271404e6c6c52a6aa9e3e894f69b43ff7343ba
DESKTOP-03O8QEC\Morgan <morgan.espindola@gmail.com>
2017-10-03 07:22:38 -0300

    Class 9 - Home work
    Change the color of slideshow guide when its changes.

--- code/assets/js/script.js
+++ code/assets/js/script.js
@@ -1,6 +1,8 @@
-window.onload = function() {
+var slideItem = 0;
 
-	setInterval(autoChangeSlide, 10000); // Function to change slides on the site
+window.onload = function() {
+	document.getElementsByClassName("guide")[0].style.backgroundColor = "#777";
+	setInterval(autoChangeSlide, 4000); // Function to change slides on the site
 
 	// Get and change the side of slides by the size of the slideshow class
 	var slideshowWidth = document.getElementById("slideshow").offsetWidth;
@@ -10,21 +12,25 @@ window.onload = function() {
 	}
 }
 
-var slideItem = 0;
 
 function autoChangeSlide() {
 	var slideshowWidth = document.getElementById("slideshow").offsetWidth;
 	if (slideItem >= 3) {
+		document.getElementsByClassName('guide')[slideItem].style.backgroundColor = '#CCC';
 		slideItem = 0;
+		document.getElementsByClassName('guide')[slideItem].style.backgroundColor = '#777';
 	} else {
+		document.getElementsByClassName('guide')[slideItem].style.backgroundColor = '#CCC';
 		slideItem ++;
 	}
-
+	document.getElementsByClassName('guide')[slideItem].style.backgroundColor = '#777';
 	document.getElementsByClassName("slideshow-area")[0].style.marginLeft = "-" + (slideshowWidth * slideItem) + "px";
 }
 
 function manualChangeSlide(pos) {
+	document.getElementsByClassName('guide')[slideItem].style.backgroundColor = '#CCC';
 	slideItem = pos;
 	var slideshowWidth = document.getElementById("slideshow").offsetWidth;
+	document.getElementsByClassName('guide')[slideItem].style.backgroundColor = '#777';
 	document.getElementsByClassName("slideshow-area")[0].style.marginLeft = "-" + (slideshowWidth * slideItem) + "px";
 }
